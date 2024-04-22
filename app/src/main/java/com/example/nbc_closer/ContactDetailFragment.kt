469c724bc3a74881abe532da18be028b85349b60
Sooyoung <agriades23@gmail.com>
2024-04-22 19:14:50 +0900

    [fix] 디테일 페이지의 레이아웃 xml 파일 연결을 R.java -> 바인딩으로 변경.

--- app/src/main/java/com/example/nbc_closer/ContactDetailFragment.kt
+++ app/src/main/java/com/example/nbc_closer/ContactDetailFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nbc_closer.databinding.FragmentContactDetailBinding
 
 class ContactDetailFragment: Fragment() {
+    private val binding by lazy { FragmentContactDetailBinding.inflate(layoutInflater) }
+
+
     //프래그먼트에 대해 레이아웃을 제공하려면 반드시 onCreateView()콜백 메서드를 구현
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_contact_detail, container,false)
+        return binding.root
     }
+
 }
