bb5d7ea42914f9919415aafd704d0da1a5595df6
Sooyoung <agriades23@gmail.com>
2024-04-22 15:00:10 +0900

    [feat] 디테일 프래그먼트를 레이아웃 파일과 연결함. - class의 형식을 Fragment로 설정했습니다. - onCreateView 함수를 오버라이딩해서, 해당하는 레이아웃 파일을 inflate해 주었습니다. - 사용한 방식은 R.java 주소 입력입니다.
    궁금한 점: fragment에 연결되는 레이아웃도 view/data binding으로 수정할 수 있나요?

--- app/src/main/java/com/example/nbc_closer/ContactDetailFragment.kt
+++ app/src/main/java/com/example/nbc_closer/ContactDetailFragment.kt
@@ -1,4 +1,18 @@
 package com.example.nbc_closer
 
-class ContactDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ContactDetailFragment: Fragment() {
+    //프래그먼트에 대해 레이아웃을 제공하려면 반드시 onCreateView()콜백 메서드를 구현
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contact_detail, container,false)
+    }
 }
