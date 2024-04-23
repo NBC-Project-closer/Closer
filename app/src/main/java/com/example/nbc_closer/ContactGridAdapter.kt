04e9c135305447df148a3ead6a0e7ec30e29cc49
JYeyun <117142316+JYeyun@users.noreply.github.com>
2024-04-23 15:23:49 +0900

    feat
    feat gird click event

--- app/src/main/java/com/example/nbc_closer/ContactGridAdapter.kt
+++ app/src/main/java/com/example/nbc_closer/ContactGridAdapter.kt
@@ -2,11 +2,12 @@ package com.example.nbc_closer
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nbc_closer.databinding.ItemContactGridBinding
 
 class ContactGridAdapter(val data:MutableList<UserData>,val onItemClick: OnItemClick):RecyclerView.Adapter<ContactGridAdapter.GridAdapter>() {
-    class GridAdapter(private val binding:ItemContactGridBinding):RecyclerView.ViewHolder(binding.root){
+    inner class GridAdapter(private val binding:ItemContactGridBinding):RecyclerView.ViewHolder(binding.root){
         fun bind(userData:UserData){
             binding.gridUserImg.setImageResource(userData.img)
             binding.gridUserName.text = userData.name
@@ -20,7 +21,18 @@ class ContactGridAdapter(val data:MutableList<UserData>,val onItemClick: OnItemC
                     userData.like = false
                 }
             }
+            if(!userData.like) binding.gridLike.setImageResource(R.drawable.icon_star)
+            else binding.gridLike.setImageResource(R.drawable.icon_fillstar)
+            binding.itemGridRecycler.setOnClickListener {
+                onItemClick.onDataClick(data[position])
+            }
         }
+    }
+    private fun setUserLike(data: UserData, img: ImageView){
+        if(!data.like)
+            img.setImageResource(R.drawable.icon_star)
+        else
+            img.setImageResource(R.drawable.icon_fillstar)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridAdapter {
         val binding = ItemContactGridBinding.inflate(LayoutInflater.from(parent.context),parent,false)
