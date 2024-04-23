dafb8ea7ae881c82b4db6d7022de3e5b18cc7a37
Sooyoung <agriades23@gmail.com>
2024-04-23 10:54:18 +0900

    [fix] 바인딩: 디테일 하단 버튼바에서 inflater.inflate(...)를 binding.root 로 교체함.

--- app/src/main/java/com/example/nbc_closer/DetailButtonBarFragment.kt
+++ app/src/main/java/com/example/nbc_closer/DetailButtonBarFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nbc_closer.databinding.FragmentDetailButtonBarBinding
 
 class DetailButtonBarFragment: Fragment() {
+    private val binding by lazy {FragmentDetailButtonBarBinding.inflate(layoutInflater)}
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail_button_bar, container, false)
+        return binding.root
     }
 }
