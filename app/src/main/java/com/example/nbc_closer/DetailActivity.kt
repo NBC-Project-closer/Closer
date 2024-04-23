5086ea196d5f83e4b21d310a556f25f4bae348e6
Sooyoung <agriades23@gmail.com>
2024-04-23 13:34:24 +0900

    [remove][fix] DetailFragment -> DetailActivity로 변경 - ContactDetailFragment.kt 및 fragment_contact_detail.xml 파일을 삭제했습니다. - 해당 내용은 DetailActivity.kt 및 activity_detail.xml로 이동해, 과도한 fragment 중첩을 줄였습니다.

--- app/src/main/java/com/example/nbc_closer/DetailActivity.kt
+++ app/src/main/java/com/example/nbc_closer/DetailActivity.kt
@@ -8,8 +8,21 @@ class DetailActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityDetailBinding.inflate(layoutInflater)
     }
+
+    //0. MainActivity.kt에서 현재 눌려진 값이 UserData의 몇 번재 항인지 받아오기.
+    private val clickedUserData = UserData(R.drawable.user_img_yujin, "안유진", "bluecar@naver.com","010-9874-3216",false)
+    //[to-do] clickedUserData에 눌려진 버튼의 UserData 값을 보내주시면 됩니다!
+    //프래그먼트에 대해 레이아웃을 제공하려면 반드시 onCreateView()콜백 메서드를 구현
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.detailImg.setImageResource(clickedUserData.img)
+        binding.detailName.text = clickedUserData.name
+        binding.detailEmail.text = clickedUserData.email
+        binding.detailNumber.text = clickedUserData.number
         setContentView(binding.root)
+
+        //http://developer.android.com/intl/ko/reference/android/content/Intent.html 문자, 또는 통화를 할 때 필요한 암시적 인텐트
+//        val messageIntent = Intent( Intent.ACTION_SENDTO, Uri.parse("tel:"+binding.detailNumber.text) )
+//        val callIntent = Intent( Intent.ACTION_DIAL, Uri.parse("sms:"+binding.detailNumber.text) )
     }
 }
