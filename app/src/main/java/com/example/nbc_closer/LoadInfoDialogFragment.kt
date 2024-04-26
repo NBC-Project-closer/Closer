eb427120dbb4cc88cdae13f7e051dfddb7f13f5f
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-04-26 14:41:26 +0900

    LoadInfoFragment.kt to LoadInfoDialogFragment.kt

--- app/src/main/java/com/example/nbc_closer/LoadInfoDialogFragment.kt
+++ app/src/main/java/com/example/nbc_closer/LoadInfoDialogFragment.kt
@@ -2,22 +2,36 @@ package com.example.nbc_closer
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
+import com.bumptech.glide.Glide
 import com.example.nbc_closer.databinding.FragmentLoadInfoBinding
+import com.example.nbc_closer.databinding.FragmentSaveInfoBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoadInfoDialogFragment : DialogFragment() {
-
+    var emailCheck = false
+    private var uri: Uri? = null
+    private lateinit var email : EditText
     private var _binding: FragmentLoadInfoBinding? = null
     private val binding get() = _binding!!
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
@@ -90,6 +104,8 @@ class LoadInfoDialogFragment : DialogFragment() {
         val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
         resultLauncher.launch(intent)
 
+        binding.loadDialogSave.isEnabled = false
+        binding.loadDialogSave.setBackgroundResource(R.drawable.dialog_button_deactivate)
         binding.loadDialogCancellation.setOnClickListener {
             binding.loadDialogName.text.clear()
             binding.loadDialogEmail.text.clear()
@@ -101,14 +117,58 @@ class LoadInfoDialogFragment : DialogFragment() {
             val name = binding.loadDialogName.text.toString()
             val email = binding.loadDialogEmail.text.toString()
             val phoneNumber = binding.loadDialogPhoneNumber.text.toString()
-            val data =
-                UserData(R.drawable.user_img_jin, name, email, phoneNumber, false,"잘 부탁드립니다!", uri = null)
+            val data = UserData(-1, name, email, phoneNumber, false, "잘 부탁드립니다!", uri)
             datalist.add(data)
+            Toast.makeText(this.context, "연락처가 추가되었습니다.", Toast.LENGTH_SHORT).show()
             binding.loadDialogName.text.clear()
             binding.loadDialogEmail.text.clear()
             binding.loadDialogPhoneNumber.text.clear()
             addSavedButtonClicked = true
             dismiss()
+        }
+
+        email = binding.loadDialogEmail
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val emailPattern = Regex("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3}+)$")
+
+                if (emailPattern.matches(email.text)) {
+                    email.error = null
+                    emailCheck = true
+                } else {
+                    email.error = ("올바르지 않는 이메일 형식")
+                    emailCheck = false
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        lifecycleScope.launch {
+            whenStarted {
+                while (true){
+                    availableSavedButton()
+                }
+            }
+        }
+    }
+
+    private suspend fun availableSavedButton() {
+        delay(500)
+         if(emailCheck){
+            binding.loadDialogSave.isEnabled = true
+            binding.loadDialogSave.setBackgroundResource(R.drawable.dialog_button)
+        }
+        else {
+            binding.loadDialogSave.isEnabled = false
+            binding.loadDialogSave.setBackgroundResource(R.drawable.dialog_button_deactivate)
         }
     }
 }
